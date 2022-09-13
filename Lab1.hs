73923f64730ad1b28727430a927eba9460ff90d4
Quickbick <nsostheller@gmail.com>
2022-09-13 01:16:36 -0700

    fixed insert and insertEvery

--- Lab1.hs
+++ Lab1.hs
@@ -7,18 +7,20 @@ module Lab1
 
 
 -- 1.insert 
-insert :: (Num t1) => t1 -> t2 -> [t2] -> [t2]
-insert n item [] = []
-insert 0 item iL = item:iL
-insert n item (i:iL) = i: (insert (n - 1) item iL)
+insert :: (Eq t1, Num t1) => t1 -> t2 -> [t2] -> [t2]
+insert n item [] | n == 0 = item:[]
+                 | otherwise = []
+insert n item (i:iL) | n == 0 = item:i:iL
+                     | otherwise = i: (insert (n - 1) item iL)
 
 -- 2. insertEvery
 insertEvery :: (Eq t, Num t) => t -> a -> [a] -> [a]
 insertEvery n item iL = insertHelper n item iL n
-where
-     insertHelper n item [] m = []
-     insertHelper 0 item iL m = item:(insertHelper (m item iL m))
-     insertHelper n item (i:iL) m = i: (insertHelper (n - 1) item iL m)
+     where
+          insertHelper n item [] m | n == 0 = item:[]
+                                   | otherwise = []
+          insertHelper n item (i:iL) m | n == 0 = item:(insertHelper m item (i:iL) m )
+                                       | otherwise = i:(insertHelper (n - 1) item iL m)
 
 -- 3. getSales
 
