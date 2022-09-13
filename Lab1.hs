8eccc18a1eddb5c17bc2287a3580b7e439f608a5
Quickbick <nsostheller@gmail.com>
2022-09-12 20:36:53 -0700

    added starter code

---
+++ Lab1.hs
@@ -0,0 +1,25 @@
+-- CptS 355 - Lab 1 (Haskell) - Fall 2022
+-- Name: 
+
+
+module Lab1
+     where
+
+
+-- 1.insert 
+
+
+-- 2. insertEvery
+
+
+-- 3. getSales
+
+                                                  
+-- 4. sumSales
+
+
+-- 5. split
+
+
+-- 6. nSplit
+
