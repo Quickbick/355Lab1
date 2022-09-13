0d8ac1b0b2889c3d21e55b5416fbc6b51b36fcf4
Quickbick <nsostheller@gmail.com>
2022-09-12 23:25:56 -0700

    added insert

--- Lab1.hs
+++ Lab1.hs
@@ -1,5 +1,5 @@
 -- CptS 355 - Lab 1 (Haskell) - Fall 2022
--- Name: 
+-- Name: Nathanael Ostheller
 
 
 module Lab1
@@ -7,7 +7,10 @@ module Lab1
 
 
 -- 1.insert 
-
+insert :: (Num t1) => t1 -> t2 -> [t2] -> [t2]
+insert (0 item iL) = item:iL
+insert (n item iL) | n > (length iL) = error "index is larger than the list length"
+insert (n item i:iL) = insert((n - 1) item iL)
 
 -- 2. insertEvery
 
