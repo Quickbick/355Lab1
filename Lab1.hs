85d6894b8c8f602e15f16ce616de33e991dabe45
Quickbick <nsostheller@gmail.com>
2022-09-13 15:49:17 -0700

    broken split

--- Lab1.hs
+++ Lab1.hs
@@ -39,7 +39,7 @@ sumSales store day ((s, (d, x):xs):xd) | store == s = (getSales day ((d, x):xs))
 split :: Eq a => a -> [a] -> [[a]] 
 split c n [] = []
 split c 0 (x:xs) = xs
-split c n (x:xs) | c == x = [] : (split c (n - 1) xs)
+split c n (x:xs) | c == x = [] : [(split c (n - 1) xs)]
 
 -- 6. nSplit
 
